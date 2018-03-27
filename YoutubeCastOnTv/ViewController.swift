da34e04890be657309f62a92953b75d18b769f4d
kirill reutov <maza51@mail.ru>
2018-03-27 20:53:45 +0300

    Initial Commit

---
+++ YoutubeCastOnTv/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  YoutubeCastOnTv
+//
+//  Created by kirill reutov on 27.03.2018.
+//  Copyright © 2018 kirill reutov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
